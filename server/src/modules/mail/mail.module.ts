26e26334e5feb94554e9d7fb02f7ad3c089a2cfc
phatnguyen03022001 <phatnguyen03022001@gmail.com>
2026-02-26 15:15:38 +0700

    Server: Fix mailler production

--- server/src/modules/mail/mail.module.ts
+++ server/src/modules/mail/mail.module.ts
@@ -9,12 +9,19 @@ import { MailService } from './mail.service';
       useFactory: (config: ConfigService) => ({
         transport: {
           host: config.get('SMTP_HOST'),
-          port: +config.get('SMTP_PORT'),
-          secure: false,
+          port: +config.get('SMTP_PORT'), // 587
+          secure: false, // false cho port 587
           auth: {
             user: config.get('SMTP_USER'),
             pass: config.get('SMTP_PASS'),
           },
+          // --- BẮT BUỘC THÊM ĐOẠN NÀY ĐỂ CHẠY TRÊN RENDER ---
+          tls: {
+            rejectUnauthorized: false, // Tránh lỗi từ chối chứng chỉ khi chạy trong Docker/Cloud
+          },
+          connectionTimeout: 10000, // Tăng thời gian chờ lên 10s
+          greetingTimeout: 10000,
+          // -----------------------------------------------
         },
         defaults: {
           from: config.get('EMAIL_FROM'),
